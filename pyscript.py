8b07372e10c493dc62efbb8df6f9186f99f31359
sandeep-menon <thesandeepmenon@gmail.com>
2018-07-10 13:50:03 +0200

    cleaning

--- pyscript.py
+++
@@ -1,8 +0,0 @@
-import random
-def randomIntegers(start, finish):
-    #returns random integers from start to finish
-    ans = []
-    for i in range(start, finish):
-        ans.append(random.randint(start, finish))
-    return ans
-    
